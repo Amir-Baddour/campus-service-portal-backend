26548669bece1fabf30fed33e05b28ff4fc890af
Amir Baddour <amirbaddour675@gmail.com>
2026-06-27 18:31:03 +0300

    Add SparkJava health endpoint and project dependencies

---
+++ src/main/java/org/campus/Config.java
@@ -0,0 +1,5 @@
+package org.campus;
+
+public class Config {
+    public static final int PORT = 8080;
+}
---
+++ src/main/java/org/campus/EnvConfig.java
@@ -0,0 +1,5 @@
+package org.campus;
+
+public class EnvConfig {
+    public static final String APP_NAME = "Campus Service Portal";
+}

47c56d9876811c42a831ac86547234ad3273e1bf
Amir Baddour <amirbaddour675@gmail.com>
2026-07-06 00:11:45 +0300

    Fix postgress pss

--- src/main/java/org/campus/EnvConfig.java
+++ src/main/java/org/campus/EnvConfig.java
@@ -5,7 +5,7 @@ public class EnvConfig {
 
     public static final String DB_URL      = getEnv("DB_URL", "jdbc:postgresql://localhost:5432/myapp");
     public static final String DB_USER     = getEnv("DB_USER", "postgres");
-    public static final String DB_PASSWORD = getEnv("DB_PASSWORD", "postgres");
+    public static final String DB_PASSWORD = getEnv("DB_PASSWORD", "AbaddourBlue@321");
 
     private static String getEnv(String key, String fallback) {
         String value = System.getenv(key);
